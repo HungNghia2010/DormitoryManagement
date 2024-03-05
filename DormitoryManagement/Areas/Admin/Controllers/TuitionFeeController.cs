f8b97f6e3df4d4d247ce26ec2529659ca6462d18
HungNghia2010 <93834571+HungNghia2010@users.noreply.github.com>
2024-03-05 23:47:43 +0700

    update

---
+++ DormitoryManagement/Areas/Admin/Controllers/TuitionFeeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DormitoryManagement.Areas.Admin.Controllers
+{
+    public class TuitionFeeController : Controller
+    {
+        // GET: Admin/TuitionFee
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
