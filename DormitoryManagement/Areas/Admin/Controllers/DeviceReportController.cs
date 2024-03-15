05782f10ec6c591358866d439424b530acf105fb
HungNghia2010 <93834571+HungNghia2010@users.noreply.github.com>
2024-03-15 14:21:50 +0700

    update

--- DormitoryManagement/Areas/Admin/Controllers/DeviceReportController.cs
+++ DormitoryManagement/Areas/Admin/Controllers/DeviceReportController.cs
@@ -13,6 +13,16 @@ namespace DormitoryManagement.Areas.Admin.Controllers
         // GET: Admin/DeviceReport
         public ActionResult Index()
         {
+            if (TempData["success"] != null)
+            {
+                ViewBag.success = TempData["success"];
+            }
+
+            if (TempData["error"] != null)
+            {
+                ViewBag.error = TempData["error"];
+            }
+
             var data = _db.DeviceReports.ToList();
 
             var room = _db.Rooms.ToList();
@@ -42,14 +52,66 @@ namespace DormitoryManagement.Areas.Admin.Controllers
 
         public ActionResult UpdateDeviceReport(int id)
         {
+
             var data = _db.DeviceReports.Find(id);
-            var room = _db.Rooms.Find(data.ID);
+            var room = _db.Rooms.Find(data.RoomId);
             var building = _db.Buildings.Find(room.BuildingID);
 
-            
+
             ViewBag.building = building.Name;
             ViewBag.room = room.Name;
             return View(data);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UpdateDeviceReport(DeviceReport deviceReport)
+        {
+            if (ModelState.IsValid)
+            {
+                var check = _db.DeviceReports.Find(deviceReport.ID);
+                var room = _db.Rooms.Find(check.RoomId);
+                var building = _db.Buildings.Find(room.BuildingID);
+                ViewBag.building = building.Name;
+                ViewBag.room = room.Name;
+
+                if (deviceReport.ReportStatus == null)
+                {
+                    ViewBag.error = "Hãy chọn trạng thái";
+                    return View();
+                }
+                var data = _db.DeviceReports.Find(deviceReport.ID);
+                data.ReportStatus = deviceReport.ReportStatus;
+                _db.SaveChanges();
+                ViewBag.success = "Cập nhật thành công";
+            }
+            return View();
+        }
+
+        public ActionResult DeleteDevice(int id)
+        {
+            // Lấy dữ liệu cần xóa từ cơ sở dữ liệu
+            var data = _db.DeviceReports.Find(id);
+            if (data == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (data.ReportStatus != "Đã hoàn thành")
+                {
+                    TempData["error"] = "Không thể xóa";
+                    return RedirectToAction("Index", "DeviceReport", new { area = "Admin" });
+                }
+                else
+                {
+                    _db.DeviceReports.Remove(data);
+                    _db.SaveChanges();
+                    TempData["success"] = "Xóa thành công";
+                    return RedirectToAction("Index", "DeviceReport", new { area = "Admin" });
+                }
+
+            }
         }
     }
 }
--- DormitoryManagement/Areas/Admin/Fonts/js/script3.js
+++ DormitoryManagement/Areas/Admin/Fonts/js/script3.js
@@ -1,56 +1,5 @@
 ﻿$(document).ready(function () {
-    $('#fromYear').on('keydown', function (e) {
-        // Chỉ cho phép nhập số và phím backspace/delete
-        if (!((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 96 && e.keyCode <= 105) || e.keyCode == 8 || e.keyCode == 46)) {
-            e.preventDefault();
-        }
-
-        // Giới hạn chiều dài của giá trị nhập vào là 4 ký tự
-        if ($(this).val().length >= 4 && e.keyCode !== 8 && e.keyCode !== 46) {
-            e.preventDefault();
-        }
-    });
-
-    $('#toYear').on('keydown', function (e) {
-        // Chỉ cho phép nhập số và phím backspace/delete
-        if (!((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 96 && e.keyCode <= 105) || e.keyCode == 8 || e.keyCode == 46)) {
-            e.preventDefault();
-        }
-
-        // Giới hạn chiều dài của giá trị nhập vào là 4 ký tự
-        if ($(this).val().length >= 4 && e.keyCode !== 8 && e.keyCode !== 46) {
-            e.preventDefault();
-        }
-    });
-
-    $("button.btn-primary").click(function () {
-        var fromMonth = $("#fromMonth").val();
-        var toMonth = $("#toMonth").val();
-        var fromYear = $("#fromYear").val();
-        var toYear = $("#toYear").val();
-
-        // Lặp qua từng hàng trong bảng
-        $("table#dataTable tbody tr").each(function () {
-            var monthYear = $(this).find("td:nth-child(3)").text(); // Lấy giá trị tháng/năm từ cột thứ 3 (thứ tự là 0-based)
-            var year = monthYear.split("/")[1];
-            var month = monthYear.split("/")[0];
-            // Kiểm tra xem tháng/năm của hàng có nằm trong khoảng từ tháng/năm đến tháng/năm không
-            if ((year >= fromYear && year <= toYear) && (month >= fromMonth && month <= toMonth)) {
-                $(this).show(); // Hiển thị hàng nếu điều kiện lọc được áp dụng
-            } else {
-                $(this).hide(); // Ẩn hàng nếu không phù hợp với điều kiện lọc
-            }
-        });
-    });
-
-    // Xử lý sự kiện khi click vào nút Reset
-    $("#resetButton").click(function () {
-        $('#fromYear').val('');
-        $('#toYear').val('');
-        $('#fromMonth').val('--Chọn tháng--');
-        $('#toMonth').val('--Chọn tháng--');
-        $("table#dataTable tbody tr").show(); // Hiển thị lại tất cả các hàng
-    });
+    
 
     $('.btn.btn-danger.btn-sm.btnstudentfee').click(function () {
         var id = $(this).data('id');
@@ -79,6 +28,18 @@
     $('#payWithMomo').click(function () {
         var id = $(this).data('id');
         window.location.href = '/Payment/PayMomo/' + id;
+    });
+
+
+    $('.btn.btn-danger.btn-sm.btndevice').click(function () {
+        var id = $(this).data('id');
+        $('#confirmDeleteDevice').data('id', id); // Lưu id của bản ghi cần xóa vào nút xác nhận xóa
+    });
+
+
+    $('#confirmDeleteDevice').click(function () {
+        var id = $(this).data('id');
+        window.location.href = '/DeviceReport/DeleteDevice/' + id;
     });
 
 });
