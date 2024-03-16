73ddc1456ea337ecf4ac4624170ea50d4a58625e
HungNghia2010 <93834571+HungNghia2010@users.noreply.github.com>
2024-03-16 21:43:09 +0700

    update

---
+++ DormitoryManagement/Areas/Admin/Controllers/SlideController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DormitoryManagement.Areas.Admin.Controllers
+{
+    public class SlideController : Controller
+    {
+        // GET: Admin/Slide
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddSlide()
+        {
+            return View();
+        }
+
+    }
+}
